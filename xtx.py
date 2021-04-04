f310d3b3331501c6d28c22afc3b590db2fdb16d9
AvitalFineRedis <avital.fine@redislabs.com>
2021-04-04 17:31:21 +0300

    adding OptionParser

--- xtx.py
+++ xtx.py
@@ -6,14 +6,6 @@ import sys
 import jinja2
 import re
 
-#parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-#                                 description='Template text Jinja2-style formatter.\nUse {{VAR}} syntax to insert variables.\nUse @include filename to include code files')
-
-#parser.add_argument('-r', '--recursive', action='store_true', help='recirsive yml-new file\nTrue by default')
-#parser.add_argument('-d', '--destinationfile', action='append',  help='name of the new yaml file')
-#parser.add_argument('-f', '--file', action='append', help='Templates file')
-#parser.add_argument('template', metavar='FILE', nargs='?', help='template file')
-#args = parser.parse_args()
 
 class parse_yml(object):
 
@@ -25,7 +17,7 @@ class parse_yml(object):
         self.vars = {}
 
 
-    def readfunction(self, file, function_name):
+    def readfunction(self, fp, function_name):
         function_content = ""
 
         # check if the name of the function is not empty
@@ -34,10 +26,10 @@ class parse_yml(object):
             sys.exit(1)
 
         # read the function content
-        line = file.readline()
+        line = fp.readline()
         while line and not line.startswith(self.__functionending_role):
             function_content += line
-            line = file.readline()
+            line = fp.readline()
 
         # insert the function content to the dictionary corresponding to the function name (the key)
         self.vars[function_name] = function_content
@@ -45,7 +37,7 @@ class parse_yml(object):
 
     def readfile(self, newfile, filename) :
         with open(filename, 'r') as fp:
-            # we use the readline methodology to be able to continue to read thr file in readfunction methode from the same 
+            # we use the readline methodology to be able to continue to read the file in readfunction methode from the same 
             # line we left this methode 
             line = fp.readline()
             while line: 
@@ -63,24 +55,26 @@ class parse_yml(object):
 if __name__ == "__main__":
     p = OptionParser()
 
-    p.add_option("-s", "--sourcefile", dest="filename", action="append", help="insert the files you want to parse")
-    p.add_option("-d", "--destinationfile", dest="filename", action="append", help="insert the destination for each file you want to parse")
+    p.add_option("-s", "--sourcefile", dest="surcefiles", action="append", help="insert the files you want to parse")
+    p.add_option("-d", "--destinationfile", dest="destinationfiles", action="append", help="insert the destination for each file you want to parse")
     #args = {
      #   "file": ["circleci.txt"],
       #  "destinationfile": ["new.yml"] 
     #}
 
     (options, args) = p.parse_args()
-    
-    if not args["file"] is None and not args["destinationfile"] is None:
-        if len(args["file"]) != len(args["destinationfile"]):
+    print(options.surcefiles)
+    print(options.destinationfiles)
+    print(type(options))
+    if not options.surcefiles is None and not options.destinationfiles is None:
+        if len(options.surcefiles) != len(options.destinationfiles):
             sys.stderr.write("The number of the source files must be equal to the number of the destination files")
             sys.exit(1)
 
         xtx = parse_yml()
 
-        for i, filename in enumerate(args["file"]):
-            newfilename = args["destinationfile"][i]
+        for i, filename in enumerate(options.surcefiles):
+            newfilename = options.destinationfiles[i]
             newfile = open(newfilename, 'r+')
             xtx.readfile(newfile, filename)
             

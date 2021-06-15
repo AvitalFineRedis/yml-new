1b6d5dda3a330fcd4babca91a12c7b1196b02e01
AvitalFineRedis <avital.fine@redislabs.com>
2021-06-15 16:15:59 +0300

    latet

--- xtx.py
+++ xtx.py
@@ -1,8 +1,9 @@
 #! /usr/bin/python
 
 from optparse import OptionParser
+from os import linesep
 import sys
-import jinja2
+import re
 
 
 class parse_yml(object):
@@ -17,7 +18,8 @@ class parse_yml(object):
         self.__functionending_role = "@end"
         self.__comment_role = "#"
         self.srcname = src_file_name
-        self.dest = open(dest_file_name, 'w+')
+        self.destname = dest_file_name
+        #self.dest = open(dest_file_name, 'w+')
         self.library = library
         self.vars = {}
 
@@ -31,7 +33,7 @@ class parse_yml(object):
 
         # check if the name of the function is not empty
         if not name:
-            sys.stderr.write("must declate a name for function")
+            sys.stderr.write("Must declare a name for function after @define")
             sys.exit(1)
 
         # read the function content
@@ -44,18 +46,28 @@ class parse_yml(object):
         # to the name (the key)
         self.vars[name] = content
 
-    def readfile(self):
-        self.parseDefinitionsAndInclude(self.srcname)
-        print(self.vars)
-        self.insertDefinitions()
+    def readFile(self):
+        for _ in range(6):
+            print(_)
+            self.dest = open(self.destname, 'w+')
+            self.parseDefinitionsAndInclude(self.srcname)
+            self.srcname = self.destname
+            print("flow:automation:release" in self.vars.keys())
+
 
     def parseDefinitionsAndInclude(self, filename):
+
         with open(filename, 'r') as fp:
             # we use the readline methodology to be able to continue to
-            # read the file in readfunction methode from the same line
+            # read the file in readDefinition methode from the same line
             # we left this methode
             line = fp.readline()
             while line:
+                
+                p = re.compile(r"^\s*")
+                result = p.search(line)
+                indentation = result.group()
+                line = line[len(indentation):]
                 if line.startswith(self.__include_role):
                     nextfilename = line[len(self.__include_role):].strip()  # isolate the name of the file
                     self.parseDefinitionsAndInclude(self.library + nextfilename)
@@ -67,26 +79,35 @@ class parse_yml(object):
                 elif line.startswith(self.__onelinebeginnig_role):
                     temp = line[len(self.__onelinebeginnig_role):].strip().split('=')
                     # if lenght of temp != 2 -> need to throw exeption
-                    name = temp[0]
-                    content = temp[1]
+                    name = temp[0].strip()
+                    content = temp[1].strip()
+                    print (name, "->", content)
                     self.vars[name] = content
+
+                elif m := re.search('{{(.+?)}}', line):
+                    name = m.group(1)
+                    if name in self.vars:
+                        value = self.vars[name]
+                        for l in value.splitlines():
+                            self.dest.write(indentation + l + "\n")
+                    else:
+                        self.dest.write(indentation + line)
 
                 elif line.startswith(self.__comment_role):
                     pass
 
                 else:
-                    self.dest.write(line)
+                    self.dest.write(indentation + line)
 
                 line = fp.readline()
 
-    def insertDefinitions(self):
-        pass
+
+
 
 if __name__ == "__main__":
     p = OptionParser()
-
-    p.add_option("-s", "--sourcefile", dest="surcefiles", action="append", help="insert the files you want to parse")
-    p.add_option("-d", "--destinationfile", dest="destinationfiles",action="append", help="insert the destination for each file you want to parse")
+    p.add_option("-s", "--sourcefile", dest="surcefiles",help="insert the files you want to parse")
+    p.add_option("-d", "--destinationfile", dest="destinationfiles", help="insert the destination for each file you want to parse")
     p.add_option("-l", "--library", dest="libraryfolder", help="")
 
     (options, args) = p.parse_args()
@@ -94,12 +115,7 @@ if __name__ == "__main__":
     print(options.destinationfiles)
 
     if options.surcefiles is not None and options.destinationfiles is not None:
-        if len(options.surcefiles) != len(options.destinationfiles):
-            sys.stderr.write("The number of the source files must be equal to the number of the destination files")
-            sys.exit(1)
-
-        for i, filename in enumerate(options.surcefiles):
-            xtx = parse_yml(filename, options.destinationfiles[i], options.libraryfolder)
-            xtx.readfile()
+        xtx = parse_yml(options.surcefiles, options.destinationfiles, options.libraryfolder)
+        xtx.readFile()
 
     exit(0)

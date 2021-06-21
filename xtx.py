3b9a12c6d60871137979dab7884cae14f59250b4
rafie <rafi@redislabs.com>
2021-06-21 10:57:16 +0300

    Fixes and comments from code review

--- xtx.py
+++ xtx.py
@@ -1,15 +1,21 @@
 #! /usr/bin/python
 
+# TODO: OptionParser is deprecated, use ArgumentParser
 from optparse import OptionParser
 from os import linesep
 import sys
 import re
 
+# import sys, os
+# sys.path.insert(0, '/w/rafi_1/readies')
+# import paella
 
-class parse_yml(object):
+
+class Gena(object):
     """
     A class for parsing fiels to yaml files
     """
+    # TODO: allow multiple libraries (include directories)
     def __init__(self, src_file_name, dest_file_name, library):
 
         self.__include_role = "@include "
@@ -33,8 +39,7 @@ class parse_yml(object):
 
         # check if the name of the function is not empty
         if not name:
-            sys.stderr.write("Must declare a name for function after @define")
-            sys.exit(1)
+            raise RuntimeException("Must declare a name for function after @define")
 
         # read the function content
         line = fp.readline()
@@ -48,22 +53,21 @@ class parse_yml(object):
 
     def readFile(self):
         for _ in range(6):
-            print(_)
+            # print(_)
             self.dest = open(self.destname, 'w+')
             self.parseDefinitionsAndInclude(self.srcname)
             self.srcname = self.destname
-            print("flow:automation:release" in self.vars.keys())
+            # print("flow:automation:release" in self.vars.keys())
 
 
     def parseDefinitionsAndInclude(self, filename):
 
         with open(filename, 'r') as fp:
             # we use the readline methodology to be able to continue to
-            # read the file in readDefinition methode from the same line
-            # we left this methode
+            # read the file in readDefinition method from the same line
+            # we left this method
             line = fp.readline()
             while line:
-                
                 p = re.compile(r"^\s*")
                 result = p.search(line)
                 indentation = result.group()
@@ -81,41 +85,52 @@ class parse_yml(object):
                     # if lenght of temp != 2 -> need to throw exeption
                     name = temp[0].strip()
                     content = temp[1].strip()
-                    print (name, "->", content)
+                    # print (name, "->", content)
                     self.vars[name] = content
 
-                elif m := re.search('{{(.+?)}}', line):
-                    name = m.group(1)
-                    if name in self.vars:
-                        value = self.vars[name]
-                        for l in value.splitlines():
-                            self.dest.write(indentation + l + "\n")
+                else:
+                    m = re.search('{{(.+?)}}', line)
+                    if m: 
+                        name = m.group(1)
+                        if name in self.vars:
+                            value = self.vars[name]
+                            for l in value.splitlines():
+                                self.dest.write(indentation + l + "\n")
+                        else:
+                            self.dest.write(indentation + line)
+
+                    elif line.startswith(self.__comment_role):
+                        pass
+
                     else:
                         self.dest.write(indentation + line)
-
-                elif line.startswith(self.__comment_role):
-                    pass
-
-                else:
-                    self.dest.write(indentation + line)
 
                 line = fp.readline()
 
 
-
-
 if __name__ == "__main__":
+    # TODO: OptionParser -> ArgumentParser
     p = OptionParser()
     p.add_option("-s", "--sourcefile", dest="surcefiles",help="insert the files you want to parse")
     p.add_option("-d", "--destinationfile", dest="destinationfiles", help="insert the destination for each file you want to parse")
     p.add_option("-l", "--library", dest="libraryfolder", help="")
 
     (options, args) = p.parse_args()
-    print(options.surcefiles)
-    print(options.destinationfiles)
+    # print(options.surcefiles)
+    # print(options.destinationfiles)
 
-    if options.surcefiles is not None and options.destinationfiles is not None:
-        xtx = parse_yml(options.surcefiles, options.destinationfiles, options.libraryfolder)
-        xtx.readFile()
+    # TODO: catch exception and return error
+    try:
+        if options.surcefiles is not None and options.destinationfiles is not None:
+            xtx = Gena(options.surcefiles, options.destinationfiles, options.libraryfolder)
+            xtx.readFile()
+    except x:
+        sys.stderr.write(f"{x}")
+        sys.exit(1)
+        
 
     exit(0)
+
+# TODOs:
+# (1) handle undefined {{...}} - print error
+# (2) operate on lists of lines instead on a file
